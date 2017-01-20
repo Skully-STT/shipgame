d22a3560867a8b79aa953421d028aa228f59e127
Skully <arachnoidea-ds@arcor.de>
2017-01-20 21:33:56 +0100

    [Added] basic lower floor prototype and movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float _speed;
+
+	Rigidbody _rigid;
+
+	void Awake()
+	{
+		_rigid = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		_rigid.MovePosition(transform.position + new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * _speed * Time.deltaTime);
+	}
+}
