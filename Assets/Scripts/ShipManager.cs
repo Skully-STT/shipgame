841c660490ddc2a0db4674ef5c3c35bf229d1a5c
Larsupulami <lars.guettinger@gmail.com>
2017-01-21 14:06:00 +0100

    GameOver

--- Assets/Scripts/ShipManager.cs
+++ Assets/Scripts/ShipManager.cs
@@ -5,8 +5,37 @@ using UnityEngine;
 
 public class ShipManager : MonoBehaviour
 {
+    private static ShipManager instance;
+
+    public static ShipManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ShipManager();
+            }
+            return instance;
+        }
+    }
+
     public int maxHealth = 100;
-    public static int shipHealth = 100;
+    private int shipHealth = 100;
+    public int Shiphealth
+    {
+        get
+        {
+            return shipHealth;
+        }
+        set
+        {
+            shipHealth = value > 0 ? value < maxHealth ? value : maxHealth:0;
+            if (shipHealth == 0)
+            {
+                GameManager.Instance.OnGameOver.Invoke();
+            }
+        }
+    }
     internal static float lostHealth = 100;
     private float lostHealtOld;
 
