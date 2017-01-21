86d7bd16e1dfd9ccc6e8cecbe58cb35a6c22ca8e
Larsupulami <lars.guettinger@gmail.com>
2017-01-21 21:37:03 +0100

    Speed UI Fix + Button Sound fix

--- Assets/Scripts/ScriptableObject/SoundFile.cs
+++ Assets/Scripts/ScriptableObject/SoundFile.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Audio;
 
 [CreateAssetMenu(fileName = "SoundFile", menuName = "Lars/Sound/AudioFile   ", order = 1)]
 public class SoundFile : ScriptableObject
 {
     public string objectName = "New SoundFile";
     public AudioClip audioClip;
+    public AudioMixerGroup audioMixerGroup;
 }
