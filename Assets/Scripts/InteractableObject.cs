000f5a90cb9128a8437bbaaf4f8ae916222ade34
Larsupulami <lars.guettinger@gmail.com>
2017-01-21 08:48:49 +0100

    [Added] 1x1 Sprite + Aktuelle Scripts

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class FloatEvent : UnityEvent<float> { }
+
+public class InteractableObject : MonoBehaviour
+{
+    public UnityEvent hitDetected;
+    public UnityEvent hitLost;
+    public FloatEvent filling;
+    public UnityEvent filled;
+    public UnityEvent empty;
+
+    public bool isHit;
+
+    [Header("Private but Serialized")]
+    [SerializeField]
+    private float maxFill = 3;
+
+    [SerializeField]
+    private float clearSpeed = 1;
+
+    [SerializeField]
+    private float fillSpeed = 1;
+
+    [Header("Private for Controll")]
+    [SerializeField]
+    private float fillState = 0;
+
+    public float FillState
+    {
+        get
+        {
+            return fillState;
+        }
+        set
+        {
+            if (value > maxFill)
+            {
+                fillState = maxFill;
+                isFilling = false;
+                filled.Invoke();
+            }
+            else if (value <= 0)
+            {
+                fillState = 0;
+                empty.Invoke();
+            }
+            else
+            {
+                filling.Invoke(fillState / maxFill);
+                fillState = value;
+            }
+        }
+    }
+
+    private bool isFilling;
+
+    public void HitDetected()
+    {
+        isHit = true;
+        hitDetected.Invoke();
+    }
+
+    public void HitLost()
+    {
+        hitLost.Invoke();
+    }
+
+    public void StartFill()
+    {
+        if (FillState < maxFill)
+            FillState += Time.deltaTime * fillSpeed;
+        else
+            FillState = maxFill;
+    }
+
+    public void StopFill()
+    {
+        if (FillState > 0)
+            FillState -= Time.deltaTime * clearSpeed;
+    }
+
+    public void Update()
+    {
+        if (!isHit)
+            hitLost.Invoke();
+
+        if (isHit&&Input.GetButton("Fire1"))
+        {
+            StartFill();
+        }
+        else
+        {
+            StopFill();
+        }
+        isHit = false;
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-	public float _speed;
+    public float _speed, _lookSpeed;
 
-	Rigidbody _rigid;
+    Rigidbody _rigid;
 
-	void Awake()
+    void Awake()
 	{
 		_rigid = GetComponent<Rigidbody>();
 	}
 
-	void FixedUpdate()
-	{
-		_rigid.MovePosition(transform.position + new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * _speed * Time.deltaTime);
-	}
+    void Update()
+    {
+        // Cam movement 
+        var r = transform.rotation;
+        var r2 = Camera.main.transform.rotation;
+
+        r *= Quaternion.Euler(new Vector3(0f, Input.GetAxis("Mouse X"), 0f));
+        r2 *= Quaternion.Euler(new Vector3(-Input.GetAxis("Mouse Y"), 0f, 0f));
+
+        Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, r2, Time.deltaTime * _lookSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, r, Time.deltaTime * _lookSpeed);
+    }
+
+
+    void FixedUpdate()
+    {
+        // Movement 
+        var v = transform.forward * Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+
+        var x = transform.right * Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+
+        _rigid.MovePosition(transform.position + v + x);
+    }
 }
