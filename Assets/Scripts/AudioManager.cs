1e95f2f2a08f6a2991e02f80dce28f06234e0c0b
Skully <arachnoidea-ds@arcor.de>
2017-01-21 18:36:39 +0100

    [Added] basic audiomanager with soundtrack and game over sound

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager Singleton { get; set; }
+
+	public AudioClip _soundtrackIntro;
+	public AudioClip _soundtrack;
+	public AudioClip _gameOver;
+
+	AudioSource _audioSource;
+
+	float _startTime;
+
+	void Awake()
+	{
+		if (Singleton == null)
+		{
+			Singleton = this;
+		}
+		else
+		{
+			throw new System.InvalidOperationException("Cannot create another instance of the 'AudioManager' class");
+		}
+
+		_audioSource = GetComponent<AudioSource>();
+	}
+
+	void Start()
+	{
+		_audioSource.clip = _soundtrackIntro;
+		_audioSource.Play();
+
+		_startTime = Time.time;
+	}
+
+	void Update()
+	{
+		if (_audioSource.clip == _soundtrack)
+		{
+			return;
+		}
+
+		if (Time.time >= _startTime + _soundtrackIntro.length)
+		{
+			_audioSource.clip = _soundtrack;
+			_audioSource.Play();
+			_audioSource.loop = true;
+		}
+	}
+
+	void OnEnable()
+	{
+		GameManager.Instance.OnGameOver += OnGameOver;
+	}
+
+	void OnDisable()
+	{
+		GameManager.Instance.OnGameOver -= OnGameOver;
+	}
+
+	public void OnGameOver()
+	{
+		print("Game Over, play game over sound");
+		_audioSource.clip = _gameOver;
+		_audioSource.Play();
+		_audioSource.loop = false;
+	}
+}
