47f138800a3d11cfa61f9c681fd81da21d377a0f
Skully <arachnoidea-ds@arcor.de>
2017-01-22 13:23:38 +0100

    [Added] rotationclamp script to clamp rotations around specified axis using quaternions

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -26,39 +26,10 @@ public class CameraMovement : MonoBehaviour {
 		rotX *= Quaternion.Euler(new Vector3(0f, Input.GetAxis("Mouse X"), 0f));
 
 		// Clamp rotation
-		rotY = ClampRotationAroundXAxis(rotY, _rotYMin, _rotYMax);
-		rotX = ClampRotationAroundYAxis(rotX, _rotXMin, _rotXMax);
+		rotY = RotationClamp.ClampAroundAxis(rotY, RotationClamp.Axis.X, _rotYMin, _rotYMax);
+		rotX = RotationClamp.ClampAroundAxis(rotX, RotationClamp.Axis.Y, _rotXMin, _rotXMax);
 
 		_camera.localRotation = Quaternion.Lerp(_camera.localRotation, rotY, Time.deltaTime * _speedX);
 		transform.localRotation = Quaternion.Lerp(transform.localRotation, rotX, Time.deltaTime * _speedY);
-		
-	}
-
-	Quaternion ClampRotationAroundYAxis(Quaternion q, float minimumY, float maximumY)
-	{
-		q.x /= q.w;
-		q.y /= q.w;
-		q.z /= q.w;
-		q.w = 1.0f;
-
-		float angleY = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.y);
-		angleY = Mathf.Clamp(angleY, minimumY, maximumY);
-		q.y = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleY);
-
-		return q;
-	}
-
-	Quaternion ClampRotationAroundXAxis(Quaternion q, float minimumX, float maximumX)
-	{
-		q.x /= q.w;
-		q.y /= q.w;
-		q.z /= q.w;
-		q.w = 1.0f;
-
-		float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-		angleX = Mathf.Clamp(angleX, minimumX, maximumX);
-		q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
-
-		return q;
 	}
 }
---
+++ Assets/Scripts/RotationClamp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationClamp {
+
+	public enum Axis
+	{
+		X,
+		Y,
+		Z
+	}
+
+	public static Quaternion ClampAroundAxis(Quaternion q, Axis axis, float minimum, float maximum)
+	{
+		q.x /= q.w;
+		q.y /= q.w;
+		q.z /= q.w;
+		q.w = 1.0f;
+
+		float angle = 2.0f * Mathf.Rad2Deg * Mathf.Atan(axis == Axis.X ? q.x : axis == Axis.Y ? q.y : q.z);
+		angle = Mathf.Clamp(angle, minimum, maximum);
+
+		switch (axis)
+		{
+			case Axis.X:
+				q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angle);
+				break;
+			case Axis.Y:
+				q.y = Mathf.Tan(0.5f * Mathf.Deg2Rad * angle);
+				break;
+			case Axis.Z:
+				q.z = Mathf.Tan(0.5f * Mathf.Deg2Rad * angle);
+				break;
+			default:
+				throw new ArgumentOutOfRangeException("axis", axis, null);
+		}
+
+		return q;
+	}
+}
